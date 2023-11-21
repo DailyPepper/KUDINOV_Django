71fcb470fd8c5a41f5c491a8de46e8b5b11099f2
DailyPepper <danielkudinov17@gmail.com>
2023-11-21 16:12:49 +0300

    start work DRF

--- kurs/urls.py
+++ kurs/urls.py
@@ -1,13 +1,14 @@
-
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
-
 from kurs import settings
+from rest_framework import routes, serializers, viewsets
 
 app_name = "KUDINOV"
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('KUDINOV.urls')),
+    path('api-auth/', include('rest_framework.urls')),
+
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
