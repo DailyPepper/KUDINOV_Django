2f769e270e9fdbad4493202cfe1900550a09e7fb
Daniel Kudinov <danielkudinov17@gmail.com>
2024-01-06 23:27:58 +0300

    add

--- kurs/urls.py
+++ kurs/urls.py
@@ -13,10 +13,11 @@ router.register(r'articles', ArticlesViewSet, basename='articles')
 
 app_name = "KUDINOV"
 urlpatterns = [
-                  path('admin/', admin.site.urls),
-                  path('', include('KUDINOV.urls')),
-                  path('api-auth/', include('rest_framework.urls'), name='api-auth'),
-                  path('api/', include(router.urls)),
-                  # path('api/', include('api.urls', namespace='api')),
-
-              ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    path('admin/', admin.site.urls),
+    path('', include('KUDINOV.urls')),
+    path('api-auth/', include('rest_framework.urls'), name='api-auth'),
+    path('api/', include(router.urls)),
+    # path('api/', include('api.urls', namespace='api')),
+    path('customer/', CustomerAPIView.as_view({'get': 'filter_user'}), name='customer-filter'),
+    path('customers/<int:pk>/custom_action_post/', CustomerAPIView.as_view({'post': 'custom_action_post'}), name='customer-custom-action-post'),
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
